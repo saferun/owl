7775f0ea07cd61a88ea8c66019d8e7538fd1d525
mel2oo <l977631253@gmail.com>
2022-05-08 00:18:26 +0800

    update locense

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,13 @@
+/*
+ * Copyright 2022 by Mel2oo <https://github.com/saferun/monitor-windows>
+ *
+ * Licensed under the GNU General Public License version 3 (GPLv3)
+ *
+ * If you distribute GPL-licensed software the license requires
+ * that you also distribute the complete, corresponding source code
+ * (as defined by GPL) to that GPL-licensed software.
+ * You should have received a copy of the GNU General Public License
+ * with this program. If not, see <https://www.gnu.org/licenses/>
+ */
+
 package main
